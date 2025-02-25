2675175e30a99f7efabfdc3660f4ae10d0cfa134
guilariel <ariluisabraham2009@gmail.com>
2025-02-25 18:11:21 -0300

    Agregar archivos de proyecto.

---
+++ cuando comprar y vender/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cuando_comprar_y_vender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = [3, 2, 6, 5, 0, 3];
+
+            MaxProfit(prices);
+            Console.ReadLine();
+
+        }
+        public static int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+            int compra = prices[0];
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (compra > prices[i])
+                {
+                    compra = prices[i];
+                }
+                if (profit < prices[i] - compra)
+                    profit = prices[i] - compra;
+            }
+
+            if (profit > 0)
+            {
+                return profit;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
+
+/*               int compra = prices[0];
+
+            for(int i = 0; i < prices.Length; i++)
+            {
+                int minVenta = prices.Skip(i).Max();
+                if (compra > prices[i] && i + 1 < prices.Length)
+                {
+                    compra = prices[i];
+                }
+
+                if (profit < minVenta - prices[i])
+                    profit = minVenta - prices[i];
+            }             for (int j = i + 1;j < prices.Length; j++)
+                {
+                    if (venta < prices[j]) 
+                    venta = prices[j];
+                    
+                    if (profit < prices[j] - prices[i])
+                    profit = venta - compra;
+
+                }
+*/
---
+++ cuando comprar y vender/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b45f621a-7242-4373-b44d-eb271e4e0380")]
